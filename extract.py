d5d6146a5a166b7004f092783ce30668a119aec3
pallaviborade1582000-lab <pallaviborade1582000@gmail.com>
2025-10-31 06:50:52 -0700

    testing

--- extract.py
+++ extract.py
@@ -1,1 +1,12 @@
+import pandas as pd
 print("Extract data from mysql")
+
+data = {
+    "id": [1,2,3],
+    "name": ["A", "B", "C"],
+    "age": [10, 20, 30]
+
+}
+
+df = pd.Dataframe(data)
+print(df)
